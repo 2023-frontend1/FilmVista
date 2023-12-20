ac6a5b8b440d4aa96606a9a21400ad80bb9557e9
yunshin <2younsin@gmail.com>
2023-12-20 17:14:54 +0900

    depend: react-query 의존성 추가 및 초기설정

---
+++ src/constants/react_query_key.js
@@ -0,0 +1,10 @@
+const movies = {
+	popular: 'MOVIES_POPULAR',
+	now_playing: 'MOVIES_NOW_PLAYING',
+	upcoming: 'MOVIES_UPCOMING',
+	top_rated: 'MOVIES_TOP_RATED',
+	search: 'MOVIES_SEARCH',
+	detail: 'MOVIE_DETAIL',
+};
+
+export default movies;
