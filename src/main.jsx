76c2d07b93154198c9fb14f8461a0c5e7dbc6496
yunshin <2younsin@gmail.com>
2023-12-20 17:14:54 +0900

    depend: react-query 의존성 추가 및 초기설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import GlobalStyles from './styles/global_styles.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import App from './App.jsx';
+import GlobalStyles from './styles/global_styles.jsx';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 60, // 1시간
+			cacheTime: 1000 * 60 * 60 * 1.5, // 1시간 30분
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GlobalStyles/>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<GlobalStyles />
+			<ReactQueryDevtools initialIsOpen={false} />
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
