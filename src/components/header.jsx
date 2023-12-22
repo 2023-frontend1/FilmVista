085c2c2782eea3e2033d99807d690bec3236d513
yunshin <2younsin@gmail.com>
2023-12-22 14:46:10 +0900

    feat: header 를 통한 페이지 이동 구현

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { color, flexAlign } from '../styles/themes/@index';
 import SearchArea from './search_area';
@@ -12,9 +13,16 @@ const Header = ({
 	$color = color.gray[900],
 	$borderColor = color.black[900],
 }) => {
+	const navigate = useNavigate();
 	return (
 		<S.Div_Wrapper {...{ $bgColor, $color, $borderColor }}>
-			<h3>FilmVista</h3>
+			<Div_WebServiceName
+				onClick={() => {
+					navigate('/');
+				}}
+			>
+				FilmVista
+			</Div_WebServiceName>
 			<SearchArea />
 		</S.Div_Wrapper>
 	);
@@ -36,8 +44,11 @@ const Div_Wrapper = styled.div`
 	border-bottom: 0.5px solid ${({ $borderColor }) => $borderColor};
 
 	${flexAlign.justifyBetween}
-
 	${flexAlign.alignCenter}
+`;
+
+const Div_WebServiceName = styled.h3`
+	cursor: pointer;
 `;
 
 const S = { Div_Wrapper };
