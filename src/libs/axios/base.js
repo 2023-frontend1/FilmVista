9dae75bcb0b3c8c7b312fd4484330e5954d45d3f
yunshin <2younsin@gmail.com>
2023-12-25 02:29:11 +0900

    fix: 환경변수 활용시 오류, axios 인스턴스를 함수의 반환값으로 변경

--- src/libs/axios/base.js
+++ src/libs/axios/base.js
@@ -6,16 +6,17 @@ import axios from 'axios';
  * - api url 과 service key 는 환경변수로 관리합니다.
  * - 언어와 지역은 한국 기준으로 설정합니다.
  */
-const axiosMovieInstance = axios.create({
-	baseURL: import.meta.env.VITE_APP_MOVIES_API_URL,
-	headers: {
-		accept: 'application/json',
-		Authorization: 'Bearer ' + import.meta.env.VITE_APP_MOVIES_API_KEY,
-	},
-	params: {
-		language: 'ko-KR',
-		region: 'KR',
-	},
-});
+const axiosMovieInstance = () =>
+	axios.create({
+		baseURL: import.meta.env.VITE_APP_MOVIES_API_URL,
+		headers: {
+			accept: 'application/json',
+			Authorization: 'Bearer ' + import.meta.env.VITE_APP_MOVIES_API_KEY,
+		},
+		params: {
+			language: 'ko-KR',
+			region: 'KR',
+		},
+	});
 
 export default axiosMovieInstance;
