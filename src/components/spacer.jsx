5fbe10c48cc9e8af4ca2b9f249317b25715c7ba5
yunshin <2younsin@gmail.com>
2023-12-25 23:12:08 +0900

    chore: Spacer 배경색상 제거

--- src/components/spacer.jsx
+++ src/components/spacer.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 /**
  * @component
  * @param {string} [$width='0px'] 가로길이 (기본값: '0px')
- * @param {string} [$height=`0px`] 세로길이 (기본값: '0px')
+ * @param {string} [$height=`0px`] 세로길이 (기본값:'0px')
  * @returns {JSX.Element}
  */
 const Spacer = ({ $width = '0px', $height = '0px' }) => {
@@ -16,6 +16,6 @@ const Div_Spacer = styled.div`
 	min-width: ${({ $width }) => $width};
 	height: ${({ $height }) => $height};
 	min-height: ${({ $height }) => $height};
-	background-color: red;
+	background-color: none;
 	display: inline-block;
 `;
